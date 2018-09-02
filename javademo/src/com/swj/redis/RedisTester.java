13eba3969a35bcdc934be254112430261bb2914a
shouwj <1132331056@qq.com>
2018-09-02 22:58:17 +0800

    redis tester

---
+++ javademo/src/com/swj/redis/RedisTester.java
@@ -0,0 +1,28 @@
+package com.swj.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by shouwj25112 on 2018/9/2.
+ */
+public class RedisTester {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.99.128", 6379, 100000);
+        int i = 0;
+        try {
+            long start = System.currentTimeMillis();// 开始毫秒数
+            while (true) {
+                long end = System.currentTimeMillis();
+                if (end - start >= 1000) {// 当大于等于1000毫秒（相当于1秒）时，结束操作
+                    break;
+                }
+                i++;
+                jedis.set("test" + i, i + "");
+            }
+        } finally {// 关闭连接
+            jedis.close();
+        }
+        // 打印1秒内对Redis的操作次数
+        System.out.println("redis每秒操作：" + i + "次");
+    }
+}
