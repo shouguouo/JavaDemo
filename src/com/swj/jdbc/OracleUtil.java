b3a5a449a79b37c6fca1be93108ced242783994c
shouwj <1132331056@qq.com>
2020-03-04 22:23:39 +0800

    using jdbc generate hsomc.one's menu sql

--- src/com/swj/jdbc/OracleUtil.java
+++ src/com/swj/jdbc/OracleUtil.java
@@ -1,14 +1,8 @@
 package com.swj.jdbc;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author swj
@@ -24,88 +18,18 @@ public class OracleUtil {
     }
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        ArrayList<BookEntity> bookEntities = new ArrayList<>();
-        Long start = System.nanoTime();
+        long start = System.currentTimeMillis();
         try {
-            connection = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "drs_user", "drs_user");
-            /*String sql = "select * from book";
-            ps = connection.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()){
-                bookEntities.add(new BookEntity(rs.getLong("bsm"), rs.getLong("book_id"),rs.getString("book_name")
-                        ,rs.getString("book_author"),rs.getString("book_pub"),rs.getString("book_price")
-                        ,rs.getString("book_sort"),rs.getString("book_status"),rs.getDate("book_record")
-                        ,rs.getLong("book_times"),rs.getString("book_publication_date")));
-            }*/
-            System.out.println(getTableInsertSql(connection, "TSYS_MENU", ""));
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:orcl", "drs_user", "drs_user");
+            new GenerateSql(connection).build();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (rs != null){
-                rs.close();
-            }
-            if (ps != null){
-                ps.close();
-            }
             if (connection != null){
                 connection.close();
             }
         }
-        System.out.println(System.nanoTime() - start);
+        System.out.println((System.currentTimeMillis() - start) / 1000.0 + "秒");
     }
-    
-    public static List<Object> getTableInsertSql(Connection conn, String tableName , String where)  
-            throws Exception {  
-        ResultSet rs = null;  
-        Statement statement = null;  
-        List<Object> list=null;  
-        try {  
-            DatabaseMetaData metadata = conn.getMetaData();  
-            rs = metadata.getColumns(null, null, tableName, "%");    //得到表的字段列表  
-  
-            String sql = "select 'insert into " + tableName + " values ( '";  
-            int count=0;  
-            int  counts=0;  
-            //获得列的总数  
-            while (rs.next()) {  
-                count++;  
-  
-            }  
-            //重新获得列数据 整理成sql  
-                rs = metadata.getColumns(null, null, tableName, "%");    //得到表的字段列表  
-                while (rs.next()) {  
-                    counts++;  
-                    if(counts<=count)  
-                    {  
-                        Object colName = rs.getObject("column_name");  
-                        sql += " ||'''' ||" + colName + "|| ''','";  
-                    }  
-                }  
-                sql=sql.substring(0,sql.length()-2)+"'";  
-                sql += " || ' );' from " + tableName+ where;  
-                rs.close();  
-   
-              // System.out.println("DEBUG: SQL="+sql);  
-                //执行  
-                statement = conn.createStatement();  
-                rs = statement.executeQuery(sql);  
-                
-                //将SQL语句放到List中  
-                list=new ArrayList<Object>();  
-                while (rs.next())  
-                    list.add(rs.getObject(1));  
-                rs.close();  
-          
-                //System.out.println(list.size());  
-            } finally {  
-                if (rs != null)  
-                    rs.close();  
-                if (statement != null)  
-                    statement.close();  
-            }  
-            return list;  
-    }  
-    
+
 }
