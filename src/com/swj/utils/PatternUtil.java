6b7c47807add7d8e5fe1badde948d4dcdb5f4a56
shouwj <1132331056@qq.com>
2020-03-26 22:30:32 +0800

    PatternUtil for regex

---
+++ src/com/swj/utils/PatternUtil.java
@@ -0,0 +1,25 @@
+package com.swj.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author wink~
+ * @date 2020/3/26 - 22:25
+ */
+public class PatternUtil {
+    public static List<String> getMatchStringList(String str, String pattern) {
+        Pattern pt = Pattern.compile(pattern);
+        Matcher match = pt.matcher(str);
+        List<String> res = new ArrayList<>();
+        while (match.find()) {
+            String matchStr = match.group().substring(1, match.group().length() -1 );
+            if (!res.contains(matchStr)) {
+                res.add(matchStr);
+            }
+        }
+        return res;
+    }
+}
