4fe46c8f70b4efdf3e1b8b4872281ce533be73c7
shouwj <1132331056@qq.com>
2020-03-29 16:04:35 +0800

    use functional interface to builder POJO

---
+++ src/com/swj/utils/Builder.java
@@ -0,0 +1,92 @@
+package com.swj.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * @author wink~
+ * @date 2020/3/19 - 22:20
+ */
+public class Builder<T> {
+
+    private final Supplier<T> instantiator;
+
+    private List<Consumer<T>> modifiers = new ArrayList<>();
+
+    public Builder(Supplier<T> instantiator) {
+        this.instantiator = instantiator;
+    }
+
+    public static <T> Builder<T> of(Supplier<T> instantiator) {
+        return new Builder<>(instantiator);
+    }
+
+    public <P1> Builder<T> with(Consumer1<T, P1> consumer, P1 p1) {
+        Consumer<T> c = instance -> consumer.accept(instance, p1);
+        modifiers.add(c);
+        return this;
+    }
+
+    public T build() {
+        T value = instantiator.get();
+        modifiers.forEach(modifier -> modifier.accept(value));
+        modifiers.clear();
+        return value;
+    }
+
+    @FunctionalInterface
+    public interface Consumer1<T, P1> {
+        void accept(T t, P1 p1);
+    }
+
+    public static void main(String[] args) {
+        Student value = Builder.of(Student::new)
+            .with(Student::setId, "1")
+            .with(Student::setName, "xhy")
+            .with(Student::setAge, 15).build();
+        System.out.println(value);
+    }
+}
+
+class Student {
+    private int age;
+
+    private String name;
+
+    private String id;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "age=" + age +
+            ", name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
