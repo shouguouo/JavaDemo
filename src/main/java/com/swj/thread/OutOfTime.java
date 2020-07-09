b4c4b69dd9442409d395b23ed907381c57472ad3
shouwj <1132331056@qq.com>
2020-07-09 21:56:02 +0800

    timer test

---
+++ src/main/java/com/swj/thread/OutOfTime.java
@@ -0,0 +1,29 @@
+package com.swj.thread;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author wink~
+ * @date 2020/7/9 - 21:52
+ */
+public class OutOfTime {
+
+    public static void main(String[] args) throws InterruptedException {
+        Timer timer = new Timer();
+        timer.schedule(new ThrowTask(), 1);
+        TimeUnit.SECONDS.sleep(1);
+        timer.schedule(new ThrowTask(), 5);
+        TimeUnit.SECONDS.sleep(5);
+    }
+}
+
+class ThrowTask extends TimerTask {
+
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+}
