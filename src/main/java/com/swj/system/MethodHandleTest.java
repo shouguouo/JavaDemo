ab0977c025b032acba300fd0f28ef6eacddc603f
shouwj <1132331056@qq.com>
2020-08-17 20:13:34 +0800

    difference between invoke and invokeExact

--- src/main/java/com/swj/system/MethodHandleTest.java
+++ src/main/java/com/swj/system/MethodHandleTest.java
@@ -18,6 +18,11 @@ public class MethodHandleTest {
         MethodType type3 = MethodType.methodType(String.class);
         Object sb3 = MethodHandles.lookup().findSpecial(Object.class, "toString", type3, MethodHandleTest.class).invoke(new MethodHandleTest());
         System.out.println(sb3);
+
+        MethodType type4 = MethodType.methodType(double.class, double.class);
+        Object sb4 = ((double) MethodHandles.lookup().findVirtual(MethodHandleTest.class, "test1", type4).invokeExact(new MethodHandleTest(), 2.0));
+        // Object sb4 = MethodHandles.lookup().findVirtual(MethodHandleTest.class, "test1", type4).invokeExact(new MethodHandleTest(), 2.0);
+        System.out.println(sb4);
     }
 
     public static int test1(String str) {
@@ -32,5 +37,9 @@ public class MethodHandleTest {
     @Override
     public String toString() {
         return hashCode() + "@" + getClass().getName();
+    }
+
+    public double test1(double i) {
+        return i * i;
     }
 }
